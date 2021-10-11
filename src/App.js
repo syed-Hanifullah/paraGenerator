0d553d9f34ece22c41406ba8571de8fcff56c8ea
syed-Hanifullah <syedhanifullah55@gmail.com>
2021-10-11 12:59:48 +0500

    lorem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import data from "./data";
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+    // console.log(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <section className="section-center">
+        <h3>
+          Type the number of Paragraphs you want
+          (max:9)
+        </h3>
+        <form
+          onSubmit={handleSubmit}
+          className="lorem-form"
         >
-          Learn React
-        </a>
-      </header>
+          <label htmlFor="amount">
+            Paragraph(s)
+          </label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            value={count}
+            onChange={(e) =>
+              setCount(e.target.value)
+            }
+          />
+          <button className="btn" type="submit">
+            Generate
+          </button>
+        </form>
+        <article className="lorem-text">
+          {text.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </article>
+      </section>
     </div>
   );
 }
